d26996052a2a50721a88c5e33650a8ccf2c8989b
André Malveira <www.carlosandre@gmail.com>
2021-12-26 18:39:21 -0300

    Realizada correções no código javascript, funcionando todas as ações, adicionar, confirmar, não confirmar e remover pessoas

--- LISTA DE CONVIDADOS/scripts.js
+++ LISTA DE CONVIDADOS/scripts.js
@@ -1,82 +1,101 @@
+let form = document.getElementById("form")
+
 let input = document.getElementById("input-principal")
 let button = document.getElementById("botao-adicionar")
 let pessoa = document.getElementById("nome-pessoa-id")
 let listaCompleta = document.getElementById("pessoas")
 
-let arrayDePessoa = []
+var arrayDePessoa = []
 recarregarPessoas()
 
 function mostrarPessoas() {
     let novali = ""
 
     arrayDePessoa.forEach((pessoa, index) => {
-
-        
-        novali = novali + `<li class="pessoa ${pessoa.confirmado == true ? "confirmado" : ""} ${pessoa.naoConfirmado == true ? "naoConfirmado" : ""}">
-        <button class="botao-confirmado" onclick="confirmarPessoa(${index})">
-            <i class="far fa-check-circle"></i>
-    </button>
-    
-    <button class="botao-nao-confirmado" onclick="naoConfirmado(${index})">
-    <i class="fas fa-window-close"></i>
-    </button>  
-    
-    <p class="nome-pessoa ${pessoa.confirmado == true ? "confirmado" : ""} ${pessoa.naoConfirmado == true ? "naoConfirmado" : ""}" id="nome-pessoa-id">${pessoa.pessoa}</p>
-        
-    <button class="botao-apagar" onclick="apagarPessoa(${index})">
-        <i class="fas fa-trash"></i>
-    </button>
-    
-        
-</li >
+        novali = novali + `
+        <li class="pessoa ${pessoa.confirmado == true ? "confirmado" : ""} ${pessoa.naoConfirmado == true ? "naoConfirmado" : ""}">
+            <button class="botao-confirmado" onclick="confirmarPessoa(${index})">
+                <i class="far fa-check-circle"></i>
+                </button>
+                
+                <button class="botao-nao-confirmado" onclick="naoConfirmado(${index})">
+                <i class="fas fa-window-close"></i>
+                </button>  
+                
+                <p class="nome-pessoa ${pessoa.confirmado == true ? "confirmado" : ""} ${pessoa.naoConfirmado == true ? "naoConfirmado" : ""}" id="nome-pessoa-id">${pessoa.pessoa}</p>
+                    
+                <button class="botao-apagar" onclick="apagarPessoa(${index})">
+                <i class="fas fa-trash"></i>
+            </button>
+        </li >
 `
     })
 
     listaCompleta.innerHTML = novali
-
-    localStorage.setItem("lista", JSON.stringify(arrayDePessoa))
 }
 
 function apagarPessoa(index){
     arrayDePessoa.splice(index, 1)
     mostrarPessoas()
-    
+
+
+    let newlista = JSON.stringify(arrayDePessoa)
+    localStorage.setItem('lista', newlista)
 }
+
 function adicionarPessoa() {
+
     arrayDePessoa.push({
     pessoa: input.value,
     status: false
     })
 
-
-    mostrarPessoas()
+    UpdateLista(arrayDePessoa)
 }
 
 function confirmarPessoa(index){
-    arrayDePessoa [index].confirmado = !arrayDePessoa [index].confirmado
-    mostrarPessoas()
+    arrayDePessoa[index].confirmado = !arrayDePessoa[index].confirmado
+
+    UpdateLista(arrayDePessoa)
+
 }
 
 function naoConfirmado (index){
     arrayDePessoa [index].naoConfirmado = !arrayDePessoa [index].naoConfirmado
-    mostrarPessoas()
+
+    UpdateLista(arrayDePessoa)
 }
 
 function recarregarPessoas(){
     let meusconvidados = localStorage.getItem("lista")
-    
-    arrayDePessoa = JSON.parse(meusconvidados)
-    
-    mostrarPessoas()
+ 
+    if(meusconvidados){
+      arrayDePessoa = JSON.parse(meusconvidados)
+      mostrarPessoas()
+    }
+
 }
 
-button.addEventListener("click", adicionarPessoa)
+function UpdateLista(arrayDePessoa) {
+  if(arrayDePessoa){
+    let lista = JSON.stringify(arrayDePessoa)
+    localStorage.setItem('lista', lista)
+    mostrarPessoas() 
+  }
 
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  adicionarPessoa()
+})
 
 function ClearFields() {
 
     document.getElementById("input-principal").value = "";
 }
+
+
 
 document.addEventListener("keypress", function(e) {
     if(e.key === 'Enter') {
@@ -86,4 +105,4 @@ document.addEventListener("keypress", function(e) {
       btn.click(one);
     
     }
-  });
+});
